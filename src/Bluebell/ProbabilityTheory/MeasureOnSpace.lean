91b7e854bed24801503c3cd1f6c612ccea00e359
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2026-02-05 17:46:22 +0100

    - Put stuff into appropriate namespaces. - Follow the naming convention more closely (not dogmatic, feel free to disregard in the future):   + defs are whateverThisCaseIsCalled   + lemmas are in_this_case   + lemmas are named e.g. like this: `lemma f_eq_add_of_P (h : P) : f a = a + b`` (i.e. conclusion_of_assumptions) where conclusion reads symbols left to right and never mentions argument names - Do not rely on autoimplicits, e.g. mention `{a b}`` in `lemma foo {a b} : a + 1 = b`` - `μ = ν.cast _` instead of the ugly `@Measure.map ...` - comments 'tied' to a def/theorem should be /-- written like this -/, this style with /-- attaches the comment to the def when you hover it

--- src/Bluebell/ProbabilityTheory/MeasureOnSpace.lean
+++ src/Bluebell/ProbabilityTheory/MeasureOnSpace.lean
@@ -15,37 +15,38 @@ noncomputable section
 
 namespace MeasureTheory
 
-@[simp]
 abbrev Measure.map! (ms₁ : MeasurableSpace α) (ms₂ : MeasurableSpace β)
                     (f : α → β) (μ : Measure α) : Measure β := @μ.map _ _ ms₁ ms₂ f
 
-@[simp]
 abbrev Measure.cast {β : Type u} {ms₁ : MeasurableSpace β}
   (μ : Measure β) (ms₂ : MeasurableSpace β) := μ.map! ms₁ ms₂ id
 
 @[simp]
-def sum (m1 : MeasurableSpace Ω) (m2 : MeasurableSpace Ω) : MeasurableSpace Ω :=
-  MeasurableSpace.generateFrom (MeasurableSet[m1] ∪ MeasurableSet[m2])
-
-def measurable_set_transport
-  (m₁ m₂ : MeasurableSpace Ω) (p : m₁ = m₂) (E : Set Ω)
-  (h : MeasurableSet[m₁] E) : MeasurableSet[m₂] E := by
-  subst p
-  simp_all only
+lemma Measure.cast_eq_self {β : Type u} {ms₁ : MeasurableSpace β}
+  (μ : Measure β) (ms₂ : MeasurableSpace β) : μ.cast ms₂ = @μ.map _ _ ms₁ ms₂ id := rfl
 
 end MeasureTheory
+
+@[simp]
+def MeasurableSpace.sum (m₁ : MeasurableSpace Ω) (m₂ : MeasurableSpace Ω) : MeasurableSpace Ω :=
+  MeasurableSpace.generateFrom (MeasurableSet[m₁] ∪ MeasurableSet[m₂])
+
+def MeasurableSpace.cast
+  (m₁ m₂ : MeasurableSpace Ω) (p : m₁ = m₂) (E : Set Ω) (h : MeasurableSet[m₁] E) :
+  MeasurableSet[m₂] E := by
+  subst p
+  simp_all only
 
 end noncomputable section
 
 /- We define `(𝓕, μ) ≤ (𝓖, ν)` if `𝓕 ⊆ 𝓖` and `μ` is the restriction of `ν` to `𝓕` -/
 @[ext]
-structure MeasureOnSpace (Ω : Type u) where
+structure MeasureOnSpace (Ω : Type*) where
   ms : MeasurableSpace Ω
   μ : Measure[ms] Ω
 
 /- Helper function to restrict the finer `MeasureOnSpace` to a coarser space -/
-def MeasureOnSpace.restrict (m₁ : MeasureOnSpace Ω) (m₂ : MeasurableSpace Ω)
-  : MeasureOnSpace Ω := {
+def MeasureOnSpace.restrict (m₁ : MeasureOnSpace Ω) (m₂ : MeasurableSpace Ω) : MeasureOnSpace Ω := {
   ms := m₂
   μ := m₁.μ.cast _
 }
@@ -56,7 +57,7 @@ instance (Ω : Type*) : Preorder (MeasureOnSpace Ω) where
   le_trans (h₁ h₂) := by
     aesop (add safe forward le_trans) (add safe (by rw [MeasureTheory.Measure.map_map]))
 
-def PSpace (Ω : Type u) :=
+def PSpace (Ω : Type*) :=
   {m : MeasureOnSpace Ω | IsProbabilityMeasure m.μ}
 
 instance (Ω : Type*) : Preorder (PSpace Ω) where
@@ -65,11 +66,11 @@ instance (Ω : Type*) : Preorder (PSpace Ω) where
   le_trans {a b c} (h₁ h₂) := by
     aesop (add safe forward le_trans) (add safe (by rw [MeasureTheory.Measure.map_map]))
 
-abbrev PSp (Ω : Type u) := WithTop (PSpace Ω)
+abbrev PSp (Ω : Type*) := WithTop (PSpace Ω)
 
-/- Holds if `r` is the independent product of `p` and `q` -/
-def isIndependentProduct (r p q : PSpace Ω) :=
-  r.1.ms = MeasureTheory.sum p.1.ms q.1.ms ∧
+/-- Holds if `r` is the independent product of `p` and `q` -/
+def PSpace.isIndependentProduct (r p q : PSpace Ω) :=
+  r.1.ms = MeasurableSpace.sum p.1.ms q.1.ms ∧
   let μ₁ := p.1.μ
   let μ₂ := q.1.μ
   let μ := r.1.μ
@@ -77,95 +78,97 @@ def isIndependentProduct (r p q : PSpace Ω) :=
     F (_ : MeasurableSet[q.1.ms] F),
   μ (E ∩ F) = μ₁ E * μ₂ F
 
-lemma MeasureOnSpace.indep_prod_has_same_measurable_set
-  (h1 : isIndependentProduct r p q) (h2 : isIndependentProduct r' p q) :
+open PSpace
+
+lemma PSPace.ms_eq_of_isIndependentProduct {r r' p q : PSpace Ω}
+  (h₁ : isIndependentProduct r p q) (h₂ : isIndependentProduct r' p q) :
   r.1.ms = r'.1.ms := by
-  rcases h1 with ⟨a, _⟩
-  rcases h2 with ⟨c, _⟩
+  rcases h₁ with ⟨a, _⟩
+  rcases h₂ with ⟨c, _⟩
   aesop
 
 section GeneratingPiSystem
 
 variable {Ω : Type*} (p q : MeasureOnSpace Ω)
 
-/-
+/--
   Given `p q : MeasureOnSpace Ω`, `generator p q` is a set of subsets of Ω that
   generates the smallest σ-algebra containing both σ-algebras
 -/
 def generator (p q : MeasureOnSpace Ω) : Set (Set Ω) :=
   {S : Set Ω | ∃ F G, S = F ∩ G ∧ MeasurableSet[p.ms] F ∧ MeasurableSet[q.ms] G}
 
-/- Every set in the generating set `w` is of the form `u ∩ v` -/
-lemma generator_pattern_match (w : Set Ω) (h_w : w ∈ generator p q) :
+/-- Every set in the generating set `w` is of the form `u ∩ v` -/
+lemma exists_inter_measurableSet_of_mem_generator (w : Set Ω) (hw : w ∈ generator p q) :
   ∃ u v, w = u ∩ v ∧ MeasurableSet[p.ms] u ∧ MeasurableSet[q.ms] v := by
-  rcases h_w with ⟨u, v, rfl, h_u, h_v⟩
+  rcases hw with ⟨u, v, rfl, hu, hv⟩
   grind
 
-lemma generator_contain_first (u : Set Ω) (h_u : MeasurableSet[p.ms] u)
-  : u ∈ generator p q := by
+lemma mem_generator_l (u : Set Ω) (hu : MeasurableSet[p.ms] u) :
+  u ∈ generator p q := by
   apply Exists.intro u
   apply Exists.intro ⊤
   simp
-  apply h_u
+  apply hu
 
-lemma generator_contain_second (u : Set Ω) (h_u : MeasurableSet[q.ms] u)
-  : u ∈ generator p q := by
+lemma mem_generator_r (u : Set Ω) (hu : MeasurableSet[q.ms] u) :
+  u ∈ generator p q := by
   apply Exists.intro ⊤
   apply Exists.intro u
   simp
-  apply h_u
+  apply hu
 
-lemma generator_elem
-  (h_u : MeasurableSet[p.ms] u) (h_v : MeasurableSet[q.ms] v)
-  : u ∩ v ∈ generator p q := by
+lemma inter_mem_generator {u v}
+  (hu : MeasurableSet[p.ms] u) (hv : MeasurableSet[q.ms] v) :
+  u ∩ v ∈ generator p q := by
   apply Exists.intro u
   apply Exists.intro v
   aesop
 
-theorem generator_generates_independent_sigma_algebra :
-  MeasurableSpace.generateFrom (generator p q) = MeasureTheory.sum p.ms q.ms := by
+theorem MeasureOnSpace.generateFrom_generator_eq_sum :
+  MeasurableSpace.generateFrom (generator p q) = p.ms.sum q.ms := by
   ext s
   constructor
   {
     apply MeasurableSpace.generateFrom_le
     simp
-    intro _ h_e
-    rcases h_e with ⟨u, ⟨v, h_elem, h_u, h_v⟩⟩
-    have h1 : u ∩ v ∈ generator p q := by
+    intro _ he
+    rcases he with ⟨u, ⟨v, helem, hu, hv⟩⟩
+    have h₁ : u ∩ v ∈ generator p q := by
       apply Exists.intro u; apply Exists.intro v; aesop
-    have h2 : generator p q ⊆ MeasurableSet[MeasureTheory.sum p.ms q.ms] := by
-      intro u h_u
-      have h_match : ∃ u1 v1, u = u1 ∩ v1 ∧ MeasurableSet[p.ms] u1 ∧ MeasurableSet[q.ms] v1 :=
-        generator_pattern_match p q u h_u
-      rcases h_match with ⟨u1, v1, rfl, h_u1, h_v1⟩
-      have h_u1_meas : MeasurableSet[MeasureTheory.sum p.ms q.ms] u1 := by
+    have h₂ : generator p q ⊆ MeasurableSet[MeasurableSpace.sum p.ms q.ms] := by
+      intro u hu
+      have hmatch : ∃ u₁ v₁, u = u₁ ∩ v₁ ∧ MeasurableSet[p.ms] u₁ ∧ MeasurableSet[q.ms] v₁ :=
+        exists_inter_measurableSet_of_mem_generator p q u hu
+      rcases hmatch with ⟨u₁, v₁, rfl, hu₁, hv₁⟩
+      have hu₁meas : MeasurableSet[MeasurableSpace.sum p.ms q.ms] u₁ := by
         apply MeasurableSpace.measurableSet_generateFrom; aesop
-      have h_v1_meas : MeasurableSet[MeasureTheory.sum p.ms q.ms] v1 := by
+      have hv₁meas : MeasurableSet[MeasurableSpace.sum p.ms q.ms] v₁ := by
         apply MeasurableSpace.measurableSet_generateFrom; aesop
-      apply MeasurableSet.inter h_u1_meas h_v1_meas
-    have h3 : MeasurableSet[MeasurableSpace.generateFrom (generator p q)] (u ∩ v) := by
+      apply MeasurableSet.inter hu₁meas hv₁meas
+    have h₃ : MeasurableSet[MeasurableSpace.generateFrom (generator p q)] (u ∩ v) := by
       apply MeasurableSpace.measurableSet_generateFrom; grind
     apply MeasurableSpace.generateFrom_le
-    exact h2
-    rw [← h_elem] at h3
-    exact h3
+    exact h₂
+    rw [← helem] at h₃
+    exact h₃
   }
   {
     apply MeasurableSpace.generateFrom_mono
     intro E hE
-    rcases hE with h1 | h2
-    apply generator_contain_first p q E h1
-    apply generator_contain_second p q E h2
+    rcases hE with h₁ | h₂
+    apply mem_generator_l p q E h₁
+    apply mem_generator_r p q E h₂
   }
 
-lemma generator_is_pi_system : IsPiSystem (generator p q) := by
+lemma MeasureOnSpace.isPiSystem_generator : IsPiSystem (generator p q) := by
   intros u h_u v h_v _
-  let ⟨u1, u2, prf_u, h_u1, h_u2⟩ := generator_pattern_match p q u h_u
-  let ⟨v1, v2, prf_v, h_v1, h_v2⟩ := generator_pattern_match p q v h_v
+  let ⟨u₁, u₂, prf_u, h_u1, h_u2⟩ := exists_inter_measurableSet_of_mem_generator p q u h_u
+  let ⟨v₁, v₂, prf_v, h_v1, h_v2⟩ := exists_inter_measurableSet_of_mem_generator p q v h_v
   subst u v
-  have h : (u1 ∩ u2) ∩ (v1 ∩ v2) = (u1 ∩ v1) ∩ (u2 ∩ v2) := by grind
+  have h : (u₁ ∩ u₂) ∩ (v₁ ∩ v₂) = (u₁ ∩ v₁) ∩ (u₂ ∩ v₂) := by grind
   rw [h]
-  apply generator_elem
+  apply inter_mem_generator
   simp_all only [MeasurableSet.inter]
   simp_all only [MeasurableSet.inter]
 
@@ -173,56 +176,54 @@ end GeneratingPiSystem
 
 section Uniqueness
 
-lemma measure_heterogeneous_ext
-  {m1 m2 : MeasurableSpace Ω}
-  (μ : Measure[m1] Ω) (ν : Measure[m2] Ω)
-  (h_eq : m1 = m2) (h2 : ∀ w, MeasurableSet[m1] w → μ w = ν w)
-  : μ = @Measure.map Ω Ω m2 m1 id ν := by
+lemma MeasurableSpace.eq_cast_ext_ext
+  {m₁ m₂ : MeasurableSpace Ω}
+  (μ : Measure[m₁] Ω) (ν : Measure[m₂] Ω)
+  (h_eq : m₁ = m₂) (h₂ : ∀ w, MeasurableSet[m₁] w → μ w = ν w) :
+  μ = ν.cast _ := by
   subst h_eq
   ext1 E h_E
-  rw [h2]
+  rw [h₂]
   congr
-  rw [Measure.map_id]
+  simp
   assumption
 
 @[ext]
-lemma measure_on_space_ext {p q : MeasureOnSpace Ω}
+lemma MeasureOnSpace.ext_ms {p q : MeasureOnSpace Ω}
   (h_eq_alg : p.ms = q.ms)
-  (h_eq_mea : ∀ E, MeasurableSet[p.ms] E → p.μ E = q.μ E)
-  : p = q := by
+  (h_eq_mea : ∀ E, MeasurableSet[p.ms] E → p.μ E = q.μ E) :
+  p = q := by
   rcases p with ⟨f, m⟩
   rcases q with ⟨g, n⟩
-  have h1 := measure_heterogeneous_ext m n h_eq_alg h_eq_mea
+  have h₁ := MeasurableSpace.eq_cast_ext_ext m n h_eq_alg h_eq_mea
   subst h_eq_alg
   simp_all only [Measure.map_id, implies_true]
 
 @[ext]
-lemma pspace_ext {p q : PSpace Ω}
+lemma PSpace.ext_ms {p q : PSpace Ω}
   (h_eq_alg : p.1.ms = q.1.ms)
-  (h_eq_mea : ∀ E, MeasurableSet[p.1.ms] E → p.1.μ E = q.1.μ E)
-  : p = q := by
+  (h_eq_mea : ∀ E, MeasurableSet[p.1.ms] E → p.1.μ E = q.1.μ E) :
+  p = q := by
   rcases p with ⟨a, _⟩
   rcases q with ⟨b, _⟩
-  have : a = b := measure_on_space_ext h_eq_alg h_eq_mea
+  have : a = b := MeasureOnSpace.ext_ms h_eq_alg h_eq_mea
   aesop
 
-lemma pspace_not_inf (m : PSpace Ω) (u : Set Ω)
-  : m.1.μ u ≠ ⊤ := by
+lemma PSpace.measure_ne_top (m : PSpace Ω) (u : Set Ω) : m.1.μ u ≠ ⊤ := by
   apply ne_of_lt
-  have h1 : m.1.μ Set.univ = 1 := m.2.measure_univ
-  have h2 : u ⊆ Set.univ := by aesop
-  have h3 : m.1.μ u ≤ m.1.μ Set.univ := measure_mono (μ := m.1.μ) h2
-  rw [h1] at h3
+  have h₁ : m.1.μ Set.univ = 1 := m.2.measure_univ
+  have h₂ : u ⊆ Set.univ := by aesop
+  have h₃ : m.1.μ u ≤ m.1.μ Set.univ := measure_mono (μ := m.1.μ) h₂
+  rw [h₁] at h₃
   apply lt_of_le_of_lt (b := 1) (c := (⊤ : ENNReal))
   simp_all only [Set.subset_univ]
   simp_all only [Set.subset_univ, ENNReal.one_lt_top]
 
-theorem uniqueness {r r' p q : PSpace Ω}
-  (h1 : isIndependentProduct r p q) (h2 : isIndependentProduct r' p q)
-  : r = r' := by
-  apply pspace_ext
-  rw [h1.1, h2.1]
-  have : IsPiSystem (generator p.1 q.1) := generator_is_pi_system p.1 q.1
+theorem PSPace.uniqueness {r r' p q : PSpace Ω}
+  (h₁ : isIndependentProduct r p q) (h₂ : isIndependentProduct r' p q) : r = r' := by
+  apply PSpace.ext_ms
+  rw [h₁.1, h₂.1]
+  have : IsPiSystem (generator p.1 q.1) := MeasureOnSpace.isPiSystem_generator p.1 q.1
   -- Applying the π-λ theorem: the σ-algebra is by definition a λ-system,
   -- so we just need to show that the measures agree on a generating π-system
   apply MeasurableSpace.induction_on_inter
@@ -230,9 +231,9 @@ theorem uniqueness {r r' p q : PSpace Ω}
   rw [MeasureTheory.measure_empty, MeasureTheory.measure_empty]
   {
     intro t h_t
-    let ⟨u, v, prf, h_u, h_v⟩ := generator_pattern_match p.1 q.1 t h_t
-    rcases h1 with ⟨h11, comb1⟩
-    rcases h2 with ⟨h21, comb2⟩
+    let ⟨u, v, prf, h_u, h_v⟩ := exists_inter_measurableSet_of_mem_generator p.1 q.1 t h_t
+    rcases h₁ with ⟨h11, comb1⟩
+    rcases h₂ with ⟨h21, comb2⟩
     rw [prf]
     have : r.1.μ (u ∩ v) = p.1.μ u * q.1.μ v := by
       apply comb1 u h_u v h_v
@@ -242,40 +243,40 @@ theorem uniqueness {r r' p q : PSpace Ω}
   }
   {
     intro u h_u prf
-    rcases h1 with ⟨h1', comb1⟩
-    rcases h2 with ⟨h2', comb2⟩
-    have is_prob1 : r.1.μ Set.univ = 1 := r.2.measure_univ
-    have is_prob2 : r'.1.μ Set.univ = 1 := r'.2.measure_univ
+    rcases h₁ with ⟨h1', comb1⟩
+    rcases h₂ with ⟨h2', comb2⟩
+    have isprob₁ : r.1.μ Set.univ = 1 := r.2.measure_univ
+    have isprob₂ : r'.1.μ Set.univ = 1 := r'.2.measure_univ
     have h : r.1.ms = r'.1.ms := by grind
-    have h_u' : MeasurableSet[r'.1.ms] u := by
-      apply measurable_set_transport r.1.ms r'.1.ms
+    have hu' : MeasurableSet[r'.1.ms] u := by
+      apply MeasurableSpace.cast r.1.ms r'.1.ms
       assumption
       assumption
-    have not_inf : r.1.μ u ≠ ⊤ := pspace_not_inf r u
-    have not_inf2 : r'.1.μ u ≠ ⊤ := pspace_not_inf r' u
+    have not_inf : r.1.μ u ≠ ⊤ := measure_ne_top r u
+    have not_inf2 : r'.1.μ u ≠ ⊤ := measure_ne_top r' u
     have : r.1.μ uᶜ = 1 - r.1.μ u := by
-      rw [MeasureTheory.measure_compl h_u not_inf, is_prob1]
+      rw [MeasureTheory.measure_compl h_u not_inf, isprob₁]
     have : r'.1.μ uᶜ = 1 - r'.1.μ u := by
-      rw [MeasureTheory.measure_compl h_u' not_inf2, is_prob2]
+      rw [MeasureTheory.measure_compl hu' not_inf2, isprob₂]
     grind
   }
   {
-    intro us disjoint h_us prf
+    intro us disjoint hus prf
     have h_sum1 : r.1.μ (⋃ i, us i) = ∑' i, r.1.μ (us i) := by
-      apply @Measure.m_iUnion (α := Ω) (f := us) r.1.ms r.1.μ h_us disjoint
+      apply @Measure.m_iUnion (α := Ω) (f := us) r.1.ms r.1.μ hus disjoint
     have h_us' : ∀ i, MeasurableSet[r'.1.ms] (us i) := by
       intro i
-      apply measurable_set_transport r.1.ms
-      apply MeasureOnSpace.indep_prod_has_same_measurable_set h1 h2
-      exact (h_us i)
+      apply MeasurableSpace.cast r.1.ms
+      apply PSPace.ms_eq_of_isIndependentProduct h₁ h₂
+      exact (hus i)
     have : r'.1.μ (⋃ i, us i) = ∑' i, r'.1.μ (us i) := by
       apply @Measure.m_iUnion (α := Ω) (f := us) r'.1.ms r'.1.μ h_us' disjoint
     grind
   }
   {
-    rcases h1 with ⟨h1, comb1⟩
-    rw [h1]
-    have := generator_generates_independent_sigma_algebra p.1 q.1
+    rcases h₁ with ⟨h₁, comb₁⟩
+    rw [h₁]
+    have := MeasureOnSpace.generateFrom_generator_eq_sum p.1 q.1
     grind
   }
 
