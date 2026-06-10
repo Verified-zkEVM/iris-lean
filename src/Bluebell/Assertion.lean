f877ac3f6e1c2a335ca0e5937aea895653cd872c
Daniel Britten <Coda-Coda@users.noreply.github.com>
2026-06-11 09:13:30 +1200

    Remove comment

--- src/Bluebell/Assertion.lean
+++ src/Bluebell/Assertion.lean
@@ -708,7 +708,7 @@ def hyperTermSemantics {Var Val : Type*} [DecidableEq Var] [Inhabited Val]
 notation "⟦" t "⟧" μ => hyperTermSemantics t μ
 
 def hyperTermReferences (t : I → Option (PSpPm Var Val → PSpPm Var Val)) : Set I :=
-  {x | (t x).isSome} -- aka `fun x => (t x).isSome` (following advice in the documentation for Set)
+  {x | (t x).isSome}
 
 /-- Short for "domain" -/
 abbrev dom (t : I → Option (PSpPm Var Val → PSpPm Var Val)) := hyperTermReferences t
