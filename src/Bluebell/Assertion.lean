163404e0ad076cd0a5cb2b7a58342a34962d6779
Daniel Britten <Coda-Coda@users.noreply.github.com>
2026-06-15 14:38:13 +1200

    Formalise the statement of WP-NEST
    The dot notation between hyper-terms t₁ and t₂, takes its definition from section 2.2 of the LHC (OOPSLA22) paper, https://doi.org/10.1145/3563298 (page 5).

--- src/Bluebell/Assertion.lean
+++ src/Bluebell/Assertion.lean
@@ -1502,7 +1502,31 @@ theorem WP_Frame
 
 -- ### WP-NEST
 
--- TODO: Rule WP-NEST spec+proof
+open Classical in
+theorem WP_Nest
+  {t₁ t₂ : I → Option (PSpPm Var Val → PSpPm Var Val)}
+  {Q : bProp I Var Val}
+  (h_no_overlap : (dom t₁) ∩ (dom t₂) = ∅) :
+  let t₁_dot_t₂ : I → Option (PSpPm Var Val → PSpPm Var Val) := (fun i : I =>
+      match h_t₁ : t₁ i with
+      | .some t₁_i =>
+        match h_t₂ : t₂ i with
+        | .some t₂_i => (
+          by
+            exfalso -- This case is unreachable due to `h_no_overlap`. So we first replace the goal with `False`, then show the contradiction. This avoids having to use a "dummy" value here (such as `.none`).
+            unfold dom hyperTermReferences at h_no_overlap
+            have : i ∈ {x | (t₁ x).isSome = true} ∩ {x | (t₂ x).isSome = true} := by
+              simp only [Set.mem_inter_iff, Set.mem_setOf_eq]
+              aesop
+            aesop
+          )
+        | .none => .some t₁_i
+      |.none =>
+        match t₂ i with
+        | .some t₂_i => .some t₂_i
+        | .none => .none)
+  wp t₁ (wp t₂ Q) ⊣⊢ wp (t₁_dot_t₂) Q := by
+    sorry -- TODO: Rule WP-NEST proof (spec not yet reviewed)
 
 -- ### WP-CONJ
 
